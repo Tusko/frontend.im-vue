da95d8d050648c3fae16cabb00742c316f9cdc10
Tusko <tusko@photoinside.me>
2019-12-19 11:55:04 +0200

    deploy added

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
+const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   configureWebpack: {
     plugins: [
-      //   new GoogleFontsPlugin({
-      //     fonts: [
-      //       { family: "Source Sans Pro", variants: ["300", "400", "600"] },
-      //       { family: "Source Code Pro", variants: ["400"] }
-      //     ]
-      //   })
+      new GoogleFontsPlugin({
+        fonts: [
+          { family: "Source Sans Pro", variants: ["300", "400", "600"] },
+          { family: "Source Code Pro", variants: ["400"] }
+        ],
+        formats: ["woff", "woff2"]
+      })
     ],
     optimization: {
       runtimeChunk: true,
